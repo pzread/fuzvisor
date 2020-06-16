503f819203926b3828fb1c540064073b27b05f1d
PZ Read <netfirewall@gmail.com>
2020-06-15 22:13:47 -0400

    Add Apache license to files.

--- third_party/compiler-rt/lib/fuzzer/FuzzerDriver.cpp
+++ third_party/compiler-rt/lib/fuzzer/FuzzerDriver.cpp
@@ -1,5 +1,7 @@
 //===- FuzzerDriver.cpp - FuzzerDriver function and flags -----------------===//
 //
+// Copyright 2020 Google LLC
+//
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
--- third_party/compiler-rt/lib/fuzzer/FuzzerInternal.h
+++ third_party/compiler-rt/lib/fuzzer/FuzzerInternal.h
@@ -1,5 +1,7 @@
 //===- FuzzerInternal.h - Internal header for the Fuzzer --------*- C++ -* ===//
 //
+// Copyright 2020 Google LLC
+//
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
